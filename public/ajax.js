6763520cff21dce2ae9e7057b7f0ba7557a10369
ouna <771565119@qq.com>
2019-05-28 19:59:46 +0800

    ts rewrite stage3

---
+++ public/ajax.js
@@ -0,0 +1,49 @@
+function ajax(url, opt = {}) {
+	let { type = 'get', headers, params = null, timeout = 0, getXhr, responseType = '' } = opt
+	return new Promise((resolve, reject) => {
+		const xhr = new XMLHttpRequest()
+		//处理get请求
+		if (type === 'get' && params && params.toString() === '[object Object]') {
+			let arr = []
+			for (let key in opt.params) arr.push(key + '=' + opt.params[key])
+			params = arr.join('&')
+			url += '?' + params
+			params = null
+		}
+		xhr.open(type, url)
+		//设置请求头
+		if (headers && headers.toString() === '[object Object]') {
+			for (let key in headers) xhr.setRequestHeader(key, headers[key])
+		}
+		//设置超时
+		xhr.timeout = timeout
+		//设置响应枚举
+		xhr.responseType = responseType
+		//监听上传进度
+		xhr.upload.onprogress = function (ev) { }
+		//处理post请求
+		if (type === 'post' && params) {
+			xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+		}
+		xhr.send(params)
+		//监听响应
+		xhr.onreadystatechange = function () {
+			if (xhr.readyState === 4) {
+				const { responseURL, status, statusText, response } = xhr
+				switch (status) {
+					case 0:
+						return reject({ url: responseURL, status, statusText: '请求超时' })
+					case 200:
+					case 206:
+						return resolve(response)
+					default:
+						return reject({ url: responseURL, status, statusText })
+				}
+			}
+		}
+		//把xhr抛出去，以便可以取消请求
+		if (getXhr) {
+			getXhr(xhr)
+		}
+	})
+}
