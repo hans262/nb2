cb988c095c673c70610676199710b67a309ec435
ouna <771565119@qq.com>
2019-05-28 19:42:55 +0800

    ws

--- src/index.js
+++
@@ -1,5 +0,0 @@
-global.REQUIRE=function REQUIRE(path){
-  return require('../'+path)
-}
-const RUN = require('../src/server/master')
-RUN()
--- start.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-@echo hello!
-node scripts/start.js
