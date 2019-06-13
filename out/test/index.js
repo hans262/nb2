b1d4136ffa10daa59e5f0ec943f8d4fc0d605a10
ounana <771565119@qq.com>
2019-06-13 17:48:09 +0800

    increase request Agent

--- out/test/index.js
+++ out/test/index.js
@@ -1,1 +1,37 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = require("http");
+const url = '/api/getuser';
+const proxy = 'http://www.baidu.com';
+const postData = 'hello world';
+const options = {
+    protocol: 'http:',
+    hostname: '127.0.0.1',
+    port: 5000,
+    path: '/api/post',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': Buffer.byteLength(postData)
+    },
+    timeout: 5000
+};
+const req = http_1.request(options, (res) => {
+    const { statusCode, headers } = res;
+    console.log(statusCode);
+    console.log(headers);
+    const chunks = [];
+    res.on('data', (chunk) => {
+        chunks.push(chunk);
+    });
+    res.on('end', () => {
+        const buffer = Buffer.concat(chunks);
+        console.log(buffer.toString());
+    });
+});
+req.on('error', (err) => {
+    console.error(err);
+});
+req.write(postData);
+req.end();
 //# sourceMappingURL=index.js.map
