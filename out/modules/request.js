2b18c04c96f66845dc6579867005ecef3d61d03a
ouna <771565119@qq.com>
2019-07-16 23:06:48 +0800

    modules it

--- out/modules/request.js
+++ out/modules/request.js
@@ -1,43 +1,51 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = require("http");
-const get = {
-    protocol: 'http:',
-    hostname: '127.0.0.1',
-    port: 5000,
-    path: '/api/get?haha=dwq',
-    method: 'GET',
-    timeout: 5000
-};
-const postData = 'hello world';
-const post = {
-    protocol: 'http:',
-    hostname: '127.0.0.1',
-    port: 5000,
-    path: '/api/post',
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': Buffer.byteLength(postData)
-    },
-    timeout: 5000
-};
-const req = http_1.request(post, (res) => {
-    const { statusCode, headers } = res;
-    console.log(statusCode);
-    console.log(headers);
-    const chunks = [];
-    res.on('data', (chunk) => {
-        chunks.push(chunk);
+function it(config) {
+    const { body, method } = config;
+    return new Promise((resolve, reject) => {
+        const req = http_1.request(config, (res) => {
+            const { statusCode } = res;
+            if (!statusCode) {
+                return reject('statusCode no exist!');
+            }
+            if (statusCode < 200 || statusCode >= 400) {
+                return reject('statusCode = ' + statusCode);
+            }
+            const chunks = [];
+            res.on('data', (chunk) => {
+                chunks.push(chunk);
+            });
+            res.on('end', () => {
+                const buffer = Buffer.concat(chunks);
+                resolve(buffer);
+            });
+        });
+        req.on('error', (err) => {
+            reject(err);
+        });
+        if (method && method === 'POST' && body) {
+            req.write(body);
+        }
+        req.end();
     });
-    res.on('end', () => {
-        const buffer = Buffer.concat(chunks);
-        console.log(buffer.toString());
-    });
-});
-req.on('error', (err) => {
-    console.error(err);
-});
-req.write(postData);
-req.end();
+}
+exports.it = it;
+;
+(async () => {
+    try {
+        const ret = await it({
+            hostname: '127.0.0.1',
+            port: 5000,
+            path: '/api/post',
+            method: 'POST',
+            timeout: 1500,
+            body: 'hello world'
+        });
+        console.log(ret.toString());
+    }
+    catch (err) {
+        console.log(err);
+    }
+})();
 //# sourceMappingURL=request.js.map
--- src/modules/request.ts
+++ src/modules/request.ts
@@ -1,51 +1,57 @@
 import { ClientRequest, IncomingMessage, request, RequestOptions } from 'http';
-/**
- * get数据以url传送
- */
-const get: RequestOptions = {
-  protocol: 'http:',
-  hostname: '127.0.0.1',
-  port: 5000,
-  path: '/api/get?haha=dwq',
-  method: 'GET',
-  timeout: 5000
+
+export interface MyRequestOptions extends RequestOptions {
+  body?: any
 }
 /**
- * post数据以流的形式发送
+ * 请求器
+ * @param config MyRequestOptions
  */
-const postData: string = 'hello world'
-const post: RequestOptions = {
-  protocol: 'http:',
-  hostname: '127.0.0.1',
-  port: 5000,
-  path: '/api/post',
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Content-Length': Buffer.byteLength(postData)
-  },
-  timeout: 5000
+export function it(config: MyRequestOptions): Promise<Buffer> {
+  const { body, method } = config
+  return new Promise<Buffer>((
+    resolve: (value?: Buffer) => void,
+    reject: (reason?: Error | string) => void
+  ) => {
+    const req: ClientRequest = request(config, (res: IncomingMessage) => {
+      const { statusCode } = res
+      if (!statusCode) {
+        return reject('statusCode no exist!')
+      }
+      if (statusCode < 200 || statusCode >= 400) {
+        return reject('statusCode = ' + statusCode)
+      }
+      const chunks: Array<Buffer> = []
+      res.on('data', (chunk: Buffer) => {
+        chunks.push(chunk)
+      })
+      res.on('end', () => {
+        const buffer: Buffer = Buffer.concat(chunks)
+        resolve(buffer)
+      })
+    })
+    req.on('error', (err: Error) => {
+      reject(err)
+    })
+    if (method && method === 'POST' && body) {
+      req.write(body)
+    }
+    req.end()
+  })
 }
 
-//发起请求
-const req: ClientRequest = request(post, (res: IncomingMessage) => {
-  const { statusCode, headers } = res
-  console.log(statusCode)
-  console.log(headers)
-  const chunks: Array<Buffer> = []
-  res.on('data', (chunk: Buffer) => {
-    chunks.push(chunk)
-  })
-  res.on('end', () => {
-    const buffer: Buffer = Buffer.concat(chunks)
-    console.log(buffer.toString())
-  })
-})
-//监听请求报错
-req.on('error', (err: Error) => {
-  console.error(err)
-})
-
-//发送数据
-req.write(postData)
-req.end()
+; (async () => {
+  try {
+    const ret = await it({
+      hostname: '127.0.0.1',
+      port: 5000,
+      path: '/api/post',
+      method: 'POST',
+      timeout: 1500,
+      body: 'hello world'
+    })
+    console.log(ret.toString())
+  } catch (err) {
+    console.log(err)
+  }
+})()
