2312791e3d7eea271e937b6332badcd7da5599cc
ounana <771565119@qq.com>
2019-07-21 17:58:35 +0800

    dwqdw

--- out/conf/packageConf.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const packageConf = require('../../package.json');
-exports.default = packageConf;
-//# sourceMappingURL=packageConf.js.map
