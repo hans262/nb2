98cc3b35ec58e2579985d939a61e7a06955b8130
ouna <771565119@qq.com>
2019-08-04 20:40:50 +0800

    dwqdq

--- out/tsdoc/index.js
+++ out/tsdoc/index.js
@@ -1,2 +1,27 @@
 "use strict";
+function test1(num) {
+    if (num <= 2) {
+        return 1;
+    }
+    else {
+        return test1(num - 1) + test1(num - 2);
+    }
+}
+function test2(num) {
+    const array = [];
+    for (let i = 0; i < num; i++) {
+        if (i > 1) {
+            array[i] = array[i - 1] + array[i - 2];
+        }
+        else {
+            array[i] = 1;
+        }
+    }
+    return array[num - 1];
+}
+const start = Date.now();
+const ret = test1(40);
+const timer = Date.now() - start;
+console.log(ret);
+console.log(timer);
 //# sourceMappingURL=index.js.map
