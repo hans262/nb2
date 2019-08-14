998d739d0a5ebbf373b60a1e9b5b7b529d227804
ounana <771565119@qq.com>
2019-08-14 17:54:12 +0800

    dwqdq

--- out/test/index.js
+++ out/test/index.js
@@ -1,36 +1,15 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const worker_threads_1 = require("worker_threads");
-const fs_1 = require("fs");
-const path_1 = require("path");
-const path_2 = require("../common/path");
-function test1() {
-    const num = 800000;
-    const writeStream = fs_1.createWriteStream(path_1.join(path_2.PUBLIC_PATH, 'text.txt'), {
-        flags: 'a'
-    });
-    const start = Date.now();
-    for (let i = 0; i < num; i++) {
-        writeStream.write('hello world ' + i + '\r\n');
+class Test {
+    constructor(size) {
+        this.size = size;
     }
-    writeStream.close();
-    writeStream.on('close', () => {
-        const timer = Date.now() - start;
-        console.log('timer: ' + timer);
-    });
-}
-function test2() {
-    const start = Date.now();
-    for (let i = 0; i < 4; i++) {
-        const worker = new worker_threads_1.Worker(__dirname + '/worker.js');
-        worker.on('error', err => {
-            console.log(err);
-        });
+    getSize() {
+        return this.size;
     }
-    process.on('exit', code => {
-        const timer = Date.now() - start;
-        console.log('timer: ' + timer);
-    });
+    say() { }
 }
-test2();
+const test = new Test('XL');
+console.log(Object.getOwnPropertyNames(Test.prototype));
+console.log(test.hasOwnProperty('size'));
+debugger;
 //# sourceMappingURL=index.js.map
--- src/test/index.ts
+++ src/test/index.ts
@@ -1,43 +1,17 @@
-/**
- * 测试多线程写文件
- * 写入200行，分四个线程去写
- */
-
-import { Worker } from 'worker_threads'
-import { createWriteStream } from 'fs';
-import { join } from 'path';
-import { PUBLIC_PATH } from '../common/path';
-
-//不使用多线程
-function test1() {
-  const num = 800000
-  const writeStream = createWriteStream(join(PUBLIC_PATH, 'text.txt'), {
-    flags: 'a'
-  })
-  const start: number = Date.now()
-  for (let i = 0; i < num; i++) {
-    writeStream.write('hello world ' + i + '\r\n')
+class Test {
+  size: string
+  constructor(size: string) {
+    this.size = size
   }
-  writeStream.close()
-  writeStream.on('close', () => {
-    const timer: number = Date.now() - start
-    console.log('timer: ' + timer)
-  })
-}
-// test1()
-
-//多线程写入
-function test2() {
-  const start: number = Date.now()
-  for (let i = 0; i < 4; i++) {
-    const worker: Worker = new Worker(__dirname + '/worker.js')
-    worker.on('error', err => {
-      console.log(err)
-    })
+  getSize(): string {
+    return this.size
   }
-  process.on('exit', code => {
-    const timer: number = Date.now() - start
-    console.log('timer: ' + timer)
-  })
+  say() { }
 }
-test2()
+
+const test = new Test('XL')
+//获取类方法
+console.log(Object.getOwnPropertyNames(Test.prototype))
+//判断类属性
+console.log(test.hasOwnProperty('size'))
+debugger
