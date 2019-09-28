e964311f0bece80e56798a148fbcb449c54269ab
ounana <771565119@qq.com>
2019-09-28 16:09:20 +0800

    ssssssssss

--- out/test/index.js
+++ out/test/index.js
@@ -1,29 +1,2 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const https_1 = require("https");
-const fs_1 = require("fs");
-const path_1 = require("path");
-const path_2 = require("../common/path");
-const options = {
-    hostname: 'www.baidu.com',
-    port: 443,
-    path: '/',
-    method: 'GET'
-};
-const req = https_1.request(options, res => {
-    console.log('状态码:', res.statusCode);
-    console.log('请求头:', res.headers);
-    const chunks = [];
-    res.on('data', (d) => {
-        chunks.push(d);
-    });
-    res.on('end', () => {
-        const data = Buffer.concat(chunks);
-        fs_1.writeFileSync(path_1.join(path_2.PUBLIC_PATH, './baidu.html'), data);
-    });
-});
-req.on('error', (e) => {
-    console.error(e);
-});
-req.end();
 //# sourceMappingURL=index.js.map
