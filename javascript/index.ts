5bbede481025d9268a40c42134c778f46ec77029
ouna <771565119@qq.com>
2021-02-14 14:14:43 +0800

    dqwdwq

--- javascript/index.ts
+++ javascript/index.ts
@@ -1,77 +1,98 @@
-
 namespace Test {
-  /**
-   * 0 0 -> 0
-   * 1 0 -> 0
-   * 0 1 -> 0
-   * 1 1 -> 1
-   */
-
   type Input = [number, number]
   type Output = number
-  const inputs: Input[] = [[0, 0], [1, 0], [0, 1], [1, 1]]
-  const outputs: Output[] = [0, 0, 0, 1]
 
-  let weights = initWeights(3)
+  const sample: [Input, Output][] = [
+    [[0, 0], 0],
+    [[1, 0], 0],
+    [[0, 1], 0],
+    [[1, 1], 1]
+  ]
   function initWeights(num: number) {
-    // 0 ~ 1
-    return new Array<number>(num).fill(0).map(v => Math.random() - 0.5)
+    return new Array(num).fill(0).map(_ => Math.random() - 0.5)
   }
+  //初始化权重
+  const weights: number[] = initWeights(3)
 
   function sigmoid(x: number) {
     return 1 / (1 + Math.pow(Math.E, -x))
   }
-
-  function calcOutput(inputs: Input[]): Output[] {
-    return inputs.map(input => {
-      let output = input[0] * weights[0] + input[1] * weights[1] + 1 * weights[2]
-      return sigmoid(output)
-    })
-  }
-  //求平均误差
-  function calcLoss(cOutputs: Output[], outputs: Output[]) {
-    const losss: number[] = []
-    cOutputs.forEach((cop, i) => {
-      let loss = Math.abs(cop - outputs[i])
-      losss.push(loss)
-    })
-    return losss.reduce((tmp, item) => tmp + item) / losss.length
+  //计算输出
+  function calcOutput(inputs: number[]) {
+    let output = inputs[0] * weights[0] + inputs[1] * weights[1] + 1 * weights[2]
+    return sigmoid(output)
   }
 
-  //计算最近n次的错误率的平均值
-  const ls: number[] = []
-  const maxError = 20
-  function calcLossp(loss: number) {
-    ls.push(loss)
-    if (ls.length > maxError) {
-      ls.shift()
+  //计算误差
+  function calcLoss(output: number, excepted: number) {
+    return Math.abs(output - excepted)
+  }
+
+  //计算20次误差平均值
+  const losss: number[] = []
+  const max = 20
+  function calcAverageloss(err: number) {
+    losss.push(err)
+    if (losss.length > max) {
+      losss.shift()
     }
-    return ls.reduce((tmp, item) => tmp + item) / ls.length
+    return losss.reduce((a, b) => a + b) / losss.length
   }
 
+  const d = 0.001 //增量
   const trainRate = 0.001 //训练率
-  const threshold = 0.001 //预值 当误差小于这个值的时候就可以
-  function train(inputs: Input[], outputs: Output[]) {
-    let cOutputs = calcOutput(inputs)
-    //计算平均误差
-    let loss = calcLoss(cOutputs, outputs)
-    //满足平均误差 小于预值的时候 就不调整误差
-    //计算下一次的误差，增加就减少权重，减少就增加权重
-    weights.forEach((_, i) => {
-      cOutputs = calcOutput(inputs)
-      const nextLoss = calcLoss(cOutputs, outputs)
-      if (nextLoss > loss) {
-        weights[i] -= trainRate
-      } else {
-        weights[i] += trainRate
-      }
+  const threshold = 0.01 //预值
+  let count = 0
+  function train(inputs: number[], excepted: number) {
+    let loss = calcLoss(calcOutput(inputs), excepted)
+    weights.forEach((w, i) => {
+      weights[i] += d
+      let loss2 = calcLoss(calcOutput(inputs), excepted)
+      const dw = (loss2 - loss) / d
+      weights[i] = w - dw * trainRate
     })
-    loss = calcLoss(calcOutput(inputs), outputs)
-    return loss
+    let evt = calcAverageloss(loss)
+    count++
+    if (count % 5000 === 0) {
+      console.log(count, evt)
+    }
+    return evt <= threshold
   }
 
-  for (let i = 0; i < 100; i++) {
-    const loss = train(inputs, outputs)
-    console.log(i + ': ' + loss)
+  for (let i = 0; ; i++) {
+    let data = sample[i % sample.length]
+    if (train(data[0], data[1])) {
+      break
+    }
   }
+
+  //验算
+  console.log(weights)
+  sample.forEach(data => {
+    console.log(calcOutput(data[0]))
+  })
+
+
+
+
+  class Model {
+    d = 0.001 //增量
+    trainRate = 0.001 //训练率
+    threshold = 0.01 //预值
+    epochs = 0
+    train() {
+
+    }
+    fit(xs: Input[], ys: Output[] ) {
+
+    }
+
+  }
+
+
+
+
+
+
+
 }
--- javascript/nns.ts
+++ javascript/nns.ts
@@ -1,16 +1,23 @@
 
 namespace Test {
-  const datas: any[] = [
+  type Input = [number, number]
+  type Output = number
+
+  const sample: [Input, Output][] = [
     [[0, 0], 0],
     [[1, 0], 0],
     [[0, 1], 0],
     [[1, 1], 1]
   ]
-  const weights: number[] = []
-  for (let i = 0; i < 3; i++) {
-    weights[i] = Math.random() - 0.5
+  function initWeights(num: number) {
+    return new Array(num).fill(0).map(_ => Math.random() - 0.5)
   }
+  //初始化权重
+  const weights: number[] = initWeights(3)
 
+  function sigmoid(x: number) {
+    return 1 / (1 + Math.pow(Math.E, -x))
+  }
   //计算输出
   function calcOutput(inputs: number[]) {
     let output = inputs[0] * weights[0] + inputs[1] * weights[1] + 1 * weights[2]
@@ -18,68 +25,51 @@ namespace Test {
   }
 
   //计算误差
-  function errRate(output: number, excepted: number) {
+  function calcLoss(output: number, excepted: number) {
     return Math.abs(output - excepted)
   }
 
-  //计算最近n次的错误率的平均值
-  const errors: number[] = []
-  const maxError = 20
-  function calcError(err: number) {
-    errors.push(err)
-    if (errors.length > maxError) {
-      errors.shift()
+  //计算20次误差平均值
+  const losss: number[] = []
+  const max = 20
+  function calcAverageloss(err: number) {
+    losss.push(err)
+    if (losss.length > max) {
+      losss.shift()
     }
-    return errors.reduce((tmp, item) => tmp + item) / errors.length
+    return losss.reduce((a, b) => a + b) / losss.length
   }
 
-  //训练
-  const d = 0.00001 //增量
-  const trainRate = 0.1 //训练率
-  const threshold = 0.001 //预值
-  let times = 1
+  const d = 0.001 //增量
+  const trainRate = 0.001 //训练率
+  const threshold = 0.01 //预值
+  let count = 0
   function train(inputs: number[], excepted: number) {
-    let err = errRate(calcOutput(inputs), excepted)
-
-    const dw: number[] = []
+    let loss = calcLoss(calcOutput(inputs), excepted)
     weights.forEach((w, i) => {
       weights[i] += d
-      let err2 = errRate(calcOutput(inputs), excepted)
-      dw[i] = (err2 - err) / d
-      weights[i] = w
+      let loss2 = calcLoss(calcOutput(inputs), excepted)
+      const dw = (loss2 - loss) / d
+      weights[i] = w - dw * trainRate
     })
-
-    weights.forEach((w, i) => {
-      weights[i] -= dw[i] * trainRate
-    })
-
-    let e = calcError(err)
-    console.log(e)
-
-    times++
-    if (times % 5000 == 0) {
-      console.log(times, e)
+    let evt = calcAverageloss(loss)
+    count++
+    if (count % 5000 === 0) {
+      console.log(count, evt)
     }
-    return e <= threshold
+    return evt <= threshold
   }
-
-  function sigmoid(x: number) {
-    return 1 / (1 + Math.pow(Math.E, -x))
-  }
-
-  train(datas[0][0], datas[0][1])
-
+  
   for (let i = 0; ; i++) {
-    let data = datas[i % datas.length]
+    let data = sample[i % sample.length]
     if (train(data[0], data[1])) {
       break
     }
   }
 
-  console.log(weights)
   //验算
-  for(let i = 0; i< datas.length; i++){
-    console.log(Math.round(calcOutput(datas[i][0])))
-  }
-
+  console.log(weights)
+  sample.forEach(data => {
+    console.log(calcOutput(data[0]))
+  })
 }
