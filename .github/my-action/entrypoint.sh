e0afeaf93509d27ddd80b6e5d4d1167c44aa5c38
hans <771565119@qq.com>
2023-06-28 10:28:30 +0800

    dqw

--- .github/my-action/entrypoint.sh
+++ .github/my-action/entrypoint.sh
@@ -4,46 +4,44 @@ set -e
 
 if [ -z "$ACCESS_TOKEN" ]
 then
-  echo "You must provide the action with a GitHub Personal Access Token secret in order to deploy."
+  echo "请设置仓库的TOKEN环境变量"
   exit 1
 fi
 
 if [ -z "$BRANCH" ]
 then
-  echo "You must provide the action with a branch name it should deploy to, for example gh-pages or docs."
+  echo "请设置发布分支"
   exit 1
 fi
 
 if [ -z "$FOLDER" ]
 then
-  echo "You must provide the action with the folder name in the repository where your compiled page lives."
+  echo "请设置资源文件夹"
   exit 1
 fi
 
 case "$FOLDER" in /*|./*)
-  echo "The deployment folder cannot be prefixed with '/' or './'. Instead reference the folder name directly."
+  echo "文件夹名，不能以/或者./开头"
   exit 1
 esac
 
 
-# 进入工作目录
+echo "进入工作目录" && \
 cd $GITHUB_WORKSPACE && \
 
-# 配置 Git.
+echo "配置git" && \
 git config --global user.email 771565119@qq.com && \
 git config --global user.name Hans && \
 
-## Initializes the repository path using the access token.
+## 初始化git仓库地址
 REPOSITORY_PATH="https://${ACCESS_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" && \
 
-# Checks to see if the remote exists prior to deploying.
-# If the branch doesn't exist it gets created here as an orphan.
-# 统计输出行数 wc -l
-# -eq 对比相等的意思
-if [ "$(git ls-remote --heads "$REPOSITORY_PATH" "$BRANCH" | wc -l)" -eq 0 ];
+# 检查发布分支是否存在，不存在则创建
+# wc -l统计输出行数，-eq对比相等
+if [ "$(git ls-remote --heads $REPOSITORY_PATH $BRANCH | wc -l)" -eq 0 ];
 then
-  echo "Creating remote branch ${BRANCH} as it doesn't exist..."
-  git checkout "${BASE_BRANCH:-master}" && \
+  echo "创建分支${BRANCH}，如果他不存在"
+  git checkout $BASE_BRANCH && \
   git checkout --orphan $BRANCH && \
   git rm -rf . && \
   touch README.md && \
@@ -53,23 +51,22 @@ then
   # 上一条命令的执行失败则退出
   if [ $? -ne 0 ];
   then
-    echo "create remote branch failed..."
+    echo "创建发布分支失败"
     exit 1
   fi
 fi
 
-# 切换到主分支
+echo "切换到分支 $BUILD_SCRIPT" && \
 git checkout $BASE_BRANCH && \
 
-# Builds the project if a build script is provided.
 echo "执行脚本 $BUILD_SCRIPT" && \
 eval "$BUILD_SCRIPT" && \
 
-# git 提交
 echo "提交git" && \
 git add -f $FOLDER && \
 git commit -m "Deploying to ${BRANCH} from $BASE_BRANCH ${GITHUB_SHA}" --quiet && \
 
+echo "发布到分支"
 # 获取docs文件夹的hash值，只提交docs文件夹到branch分支
 git push $REPOSITORY_PATH `git subtree split --prefix $FOLDER $BASE_BRANCH`:$BRANCH --force && \
 echo "发布成功"
